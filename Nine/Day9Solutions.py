8bf914a48294347b8da7f42b5c7aef10e665e76d
Caleb Howard <cjhosu@gmail.com>
2020-12-09 22:34:47 -0500

    Much refactor!

--- Nine/Day9Solutions.py
+++ Nine/Day9Solutions.py
@@ -3,11 +3,10 @@ target = 0
 with open('Day9_input') as file: 
     for line in file:
         line = line.strip()
-        nums.append(line)
+        nums.append(int(line))
 
 
 class Validator:
-    current_added = 0
     done = None
 
     def __init__(self, nums, idx):
@@ -19,34 +18,27 @@ class Validator:
         self.current = nums[idx]
         self.checkset = nums[idx-25:idx]
         if not self.now_check(self.current, self.checkset):
+            print(self.current)
             global target
             target = self.current
-            print(self.current)
             return
 
     def now_check(self,current, checkset):
         for each in checkset:
-            if str(int(current) - int(each)) in checkset:
+            if current - each in checkset:
                 return True
 
-    def adder(self, range_added):
-        global target
-        target = int(target)
-        new_index = int(self.idx)  + 1
-        if new_index == len(nums):
-            return
+    def adder(self, range_added, target):
         range_added.append(nums[self.idx])
-        self.current_added += int(nums[self.idx])
-        self.idx = new_index
-        if self.current_added < target:
-            self.adder(range_added)
-        elif self.current_added > target:
+        self.idx += 1
+        if sum(range_added) < target:
+            self.adder(range_added, target)
+        elif sum(range_added) > target:
             range_added = []
             self.current_added = 0
             return
-        elif self.current_added == target and len(range_added) > 1:
-            range_added.sort()
-            print(range_added)
+        elif sum(range_added) == target and len(range_added) > 1:
+            print(min(range_added) + max(range_added))
             self.done = True
 
 for idx in range(25,len(nums)):
@@ -55,8 +47,6 @@ for idx in range(25,len(nums)):
 
 for idx in range(0,len(nums)):
     v = Validator(nums, idx)
-    if v.done != True:
+    if not v.done:
         range_added = []
-        v.adder(range_added)
-    else:
-        break
+        v.adder(range_added, target)
